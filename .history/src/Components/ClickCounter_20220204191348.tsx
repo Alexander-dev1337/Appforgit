f69ed33d82344fca30a9b7685eca4ba3262e16fb
joshua <joshuap12321@gmail.com>
2022-02-04 19:15:15 -0500

    Yay i got help!

---
+++ .history/src/Components/ClickCounter_20220204191348.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function ClickCounter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>
+        Count:&nbsp; <span>{count}</span>
+      </h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  );
+}
